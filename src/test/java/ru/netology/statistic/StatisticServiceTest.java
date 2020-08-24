5c49cb8c2e6ecfb61f75a5f2112d20fa6b572789
syan91 <syan199165@gmail.com>
2020-08-24 23:06:13 +0300

    Adding test for 100% coverage

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -17,5 +17,15 @@ class StatisticServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxAdd() {
+        StatisticService service = new StatisticService();
 
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
